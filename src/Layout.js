78c705bdbc97914db2f58265724d7534d1f485e1
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2021-02-12 10:21:49 -0300

    Refactor Drawer

--- src/Layout.js
+++ src/Layout.js
@@ -3,21 +3,12 @@ import clsx from 'clsx';
 import { useHistory } from "react-router-dom";
 
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import AppsIcon from '@material-ui/icons/Apps';
 import CloudQueueIcon from '@material-ui/icons/CloudQueue';
@@ -30,7 +21,9 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import {AccountCircle ,More} from "@material-ui/icons";
 import {FormControl, InputLabel, ListSubheader, Select} from "@material-ui/core";
 
-const drawerWidth = 240;
+import DrawerMenu from "./components/DrawerMenu";
+
+import {drawerWidth} from "./constants/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -321,39 +314,7 @@ const Layout = ({ children, ...props}) => {
         </AppBar>
         {renderMobileMenu}
         {renderMenu}
-        <Drawer
-            className={classes.drawer}
-            variant="persistent"
-            anchor="left"
-            open={open}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-        >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-            ))}
-          </List>
-        </Drawer>
+        <DrawerMenu open={open} handleDrawerClose={handleDrawerClose} />
         <main
             className={clsx(classes.content, {
               [classes.contentShift]: open,
