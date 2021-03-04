a558d14d9d3c8b3148e7624ca7ec424210550f39
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2021-03-04 18:10:55 -0300

    Generated automatically login

---
+++ src/helper/storage.js
@@ -0,0 +1,20 @@
+export const setObjectOn = (key, data) => {
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
+export const getObjectFrom = (key) => {
+  const data = localStorage.getItem(key);
+  return data? JSON.parse(data):data;
+};
+
+export const setPlainOn = (key, data) => {
+  localStorage.setItem(key, data);
+};
+
+export const getPlainFrom = (key) => {
+  return localStorage.getItem(key);
+};
+
+export const removeKey = (key) => {
+  localStorage.removeItem(key);
+};
