b2668a204caf8ea1a2d6b56399b6529ab95292e5
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2021-04-23 13:50:25 -0300

    Added functionalities without behavior

--- src/components/CecocloudModulesSelector/index.js
+++ src/components/CecocloudModulesSelector/index.js
@@ -1,9 +1,10 @@
-import {compose} from "redux";
+import {bindActionCreators, compose} from "redux";
 import {connect} from "react-redux";
 import {injectIntl} from "react-intl";
 
 import CecocloudModulesSelector from "./CecocloudModulesSelector";
 import {getLoading, getModules} from "../../redux/modules/selectors";
+import {loadModule} from "../../redux/modules";
 
 const mapStateToProps = (state, props) => {
   return {
@@ -12,7 +13,14 @@ const mapStateToProps = (state, props) => {
   };
 };
 
+const mapDispatchToProps = (dispatch, props) => {
+  const actions = {
+    selectModule: bindActionCreators(loadModule,dispatch),
+  };
+  return { actions };
+};
+
 export default compose(
-  connect(mapStateToProps,null),
+  connect(mapStateToProps,mapDispatchToProps),
   injectIntl
 )(CecocloudModulesSelector);
