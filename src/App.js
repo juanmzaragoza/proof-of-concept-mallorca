fe8a60c18c787c259e463c9e41e8fcb5211b1c2d
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2021-05-14 09:53:11 -0300

    Refactor index.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ import {
 } from 'react-router-dom';
 import {bindActionCreators, compose} from 'redux';
 import {connect} from "react-redux";
-import { SnackbarProvider } from 'notistack';
 
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import MomentUtils from '@date-io/moment';
-
-import { theme } from 'constants/theme';
 import * as ROUTES from "constants/routes";
 import { Login } from 'modules/Authentication';
 import Layout from "./Layout";
@@ -21,7 +15,6 @@ import {isUserAuthenticated} from "helper/login-helper";
 
 import {getAuthenticated, getAuthenticationError, getLoading} from "./redux/app/selectors";
 import {authenticate, clearAfterAuthentication} from "./redux/app";
-import {SnackbarUtilsConfigurator} from "./helper/snackbar-function";
 
 const mapStateToProps = (state, props) => {
   return {
@@ -44,50 +37,34 @@ const LoginComponent = compose(
 )(Login);
 
 function App() {
-  //login();
   return (
-      <Router>
-        <MuiThemeProvider theme={theme}>
-          <MuiPickersUtilsProvider utils={MomentUtils}>
-              <SnackbarProvider
-                maxSnack={4}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'right',
-                }}
-            >
-              <SnackbarUtilsConfigurator />
-              <div>
-                <Switch>
-                  <Route exact path={ROUTES.LOGIN}>
-                    <LoginComponent />
-                  </Route>
-                  <Route path='/' component={Layout} />
-                </Switch>
-                {/*TODO() -> conectar hooks en vez de redux
-                TODO() https://medium.com/octopus-labs-london/replacing-redux-with-react-hooks-and-context-part-1-11b72ffdb533
-                app.loading && (
-                    <div
-                        style={{
-                          position: 'fixed',
-                          display: 'flex',
-                          justifyContent: 'center',
-                          alignItems: 'center',
-                          height: '100vh',
-                          width: '100vw',
-                          zIndex: '1202',
-                          top: 0,
-                          backgroundColor: 'rgba(0,0,0, 0.6)',
-                        }}
-                    >
-                      <CircularProgress color='secondary' size={80} />
-                    </div>
-                )*/}
-              </div>
-            </SnackbarProvider>
-          </MuiPickersUtilsProvider>
-        </MuiThemeProvider>
-      </Router>
+    <Router>
+      <Switch>
+        <Route exact path={ROUTES.LOGIN}>
+          <LoginComponent />
+        </Route>
+        <Route path='/' component={Layout} />
+      </Switch>
+      {/*TODO() -> connect hooks instead of redux
+      TODO() https://medium.com/octopus-labs-london/replacing-redux-with-react-hooks-and-context-part-1-11b72ffdb533
+      app.loading && (
+          <div
+              style={{
+                position: 'fixed',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                width: '100vw',
+                zIndex: '1202',
+                top: 0,
+                backgroundColor: 'rgba(0,0,0, 0.6)',
+              }}
+          >
+            <CircularProgress color='secondary' size={80} />
+          </div>
+      )*/}
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,18 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {IntlProvider} from "react-intl";
 import {Provider} from "react-redux";
+import {MuiThemeProvider} from "@material-ui/core";
+import {MuiPickersUtilsProvider} from "@material-ui/pickers";
+import {SnackbarProvider} from "notistack";
+import MomentUtils from "@date-io/moment";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import messages from 'assets/i18n/es/locale.json';
 import store from "redux/store.js";
+import {theme} from "constants/theme";
+import {SnackbarUtilsConfigurator} from "./helper/snackbar-function";
 
 //TODO() itntl https://www.freecodecamp.org/news/setting-up-internationalization-in-react-from-start-to-finish-6cb94a7af725/
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <IntlProvider messages={messages} locale={'es'}>
-        <App />
+        <MuiThemeProvider theme={theme}>
+          <MuiPickersUtilsProvider utils={MomentUtils}>
+            <SnackbarProvider
+              maxSnack={4}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+            >
+              <SnackbarUtilsConfigurator />
+              <App />
+            </SnackbarProvider>
+          </MuiPickersUtilsProvider>
+        </MuiThemeProvider>
       </IntlProvider>
     </Provider>
   </React.StrictMode>,
