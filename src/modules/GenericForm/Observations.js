ead6be902381555e3a5db135e12aad120df8add3
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2021-05-24 11:36:43 -0300

    Observations component

---
+++ src/modules/GenericForm/Observations.js
@@ -0,0 +1,53 @@
+import {useState} from "react";
+import PropTypes from "prop-types";
+import LabelIcon from '@material-ui/icons/Label';
+import LabelOffIcon from '@material-ui/icons/LabelOff';
+import IconButton from "@material-ui/core/IconButton";
+import ObservationsForm from "./ObservationsForm";
+
+const Observations = ({id, placeHolder, required, disabled, value = '', onChange, ...props}) => {
+  const [open, setOpen] = useState(false);
+
+  const isEmpty = () => !value || !value.length;
+  const EmptyButton = (
+    isEmpty()?
+      <IconButton onClick={() => setOpen(true)} disabled={disabled} color="primary" aria-label="upload picture" component="span">
+        <LabelOffIcon />
+      </IconButton>
+      :
+      null
+  );
+
+  const FullButton = (
+    !isEmpty()?
+      <IconButton onClick={() => setOpen(true)} disabled={disabled} color="primary" aria-label="upload picture" component="span">
+        <LabelIcon />
+      </IconButton>
+      :
+      null
+  );
+
+  return <div>
+    {EmptyButton}
+    {FullButton}
+    <ObservationsForm open={open}
+                      required={required}
+                      onClose={() => setOpen(false)}
+                      onSubmit={(e,value) => {
+                        onChange(e,value);
+                      }}
+                      value={value}
+                      placeHolder={placeHolder} />
+  </div>;
+}
+
+Observations.propTypes = {
+  id: PropTypes.any,
+  required: PropTypes.bool,
+  disabled: PropTypes.bool,
+  placeHolder: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
+
+export default Observations;
---
+++ src/modules/GenericForm/ObservationsForm.js
@@ -0,0 +1,97 @@
+import React, {useEffect, useState} from "react";
+import PropTypes from "prop-types";
+import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@material-ui/core";
+import {FormattedMessage, injectIntl} from "react-intl";
+import GenericForm from "./index";
+import Button from "@material-ui/core/Button";
+import {compose} from "redux";
+
+const ObservationsForm = ({open, onClose = () => {}, onSubmit, value = '', placeHolder, required, multiline = 4, ...props}) => {
+  const [openModal, setOpenModal] = useState(open);
+  const [text, setText] = useState(value);
+
+  useEffect(()=>{
+    setOpenModal(open);
+  },[open]);
+
+  const handleClose = () => {
+    setOpenModal(false);
+    onClose();
+  };
+
+  const formComponents = [
+    {
+      placeHolder: placeHolder? placeHolder:props.intl.formatMessage({
+        id: "FamiliaProveedores.observaciones",
+        defaultMessage: "Observaciones"
+      }),
+      type: 'input',
+      key: 'observacions',
+      breakpoints: {
+        xs: 12,
+        md: 12
+      },
+      text: {
+        multiline: multiline
+      }
+    }
+  ]
+
+  return(
+    <Dialog
+      open={openModal}
+      onClose={() => handleClose()}
+      aria-labelledby="obs-dialog-title"
+      fullWidth={true}
+      maxWidth={"md"}
+    >
+      <DialogTitle id="form-dialog-title">
+        <FormattedMessage id={"Observaciones.titulo"} defaultMessage={"Información extra"} />
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          <FormattedMessage id={"Observaciones.subtitulo"} defaultMessage={"Para agregar una descripción, por favor complete la información que aparecen a continuació"}/>
+        </DialogContentText>
+      </DialogContent>
+      <GenericForm
+        formComponents={formComponents}
+        getFormData={(key) => text}
+        setFormData={({key, value}) => setText(value)}
+        containerSpacing={0}
+        fieldsContainerStyles={{padding: "0px 10px 0px 10px"}}
+      />
+      <DialogActions>
+        <Button
+          onClick={() => handleClose()}
+          color="primary"
+          disabled={props.loading} >
+          <FormattedMessage id={"Forms.cancel"} defaultMessage={"Cancelar"} />
+        </Button>
+        <Button
+          onClick={(e) => {
+            onSubmit(e, text);
+            handleClose();
+          }}
+          disabled={props.loading}
+          color="primary"
+          disabled={required? !text || !text.length:false}>
+          <FormattedMessage id={"Forms.guardar"} defaultMessage={"Guardar"} />
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+};
+
+ObservationsForm.propTypes = {
+  open: PropTypes.bool,
+  required: PropTypes.bool,
+  onClose: PropTypes.func,
+  onSubmit: PropTypes.func,
+  value: PropTypes.string,
+  placeHolder: PropTypes.string,
+  multiline: PropTypes.number
+}
+
+export default compose(
+  injectIntl
+)(ObservationsForm);
