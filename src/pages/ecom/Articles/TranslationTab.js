85fbae8c90dddc3e1a695f4798f47a3412b910fc
amula-limit <amula@limit.es>
2021-07-15 09:33:23 +0200

    Añadir DescripcionCorta en ecom/articulos/traduccion

--- src/pages/ecom/Articles/TranslationTab.js
+++ src/pages/ecom/Articles/TranslationTab.js
@@ -1,34 +1,26 @@
-import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-import {FormattedMessage, injectIntl} from "react-intl";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { FormattedMessage, injectIntl } from "react-intl";
 import Grid from "@material-ui/core/Grid/Grid";
-
-
-
 
 import OutlinedContainer from "modules/shared/OutlinedContainer";
 
-import {compose} from "redux";
-import {withValidations} from "modules/wrappers";
+import { compose } from "redux";
+import { withValidations } from "modules/wrappers";
 import ExpandableGrid from "modules/ExpandableGrid";
 
-import {useTabForm} from "hooks/tab-form";
+import { useTabForm } from "hooks/tab-form";
 
+const TranslationTab = ({ formData, setFormData, getFormData, ...props }) => {
+  const [touched, handleTouched, addValidity, formIsValid] = useTabForm({
+    fields: { 0: false, 1: false },
+    setIsValid: props.setIsValid,
+  });
 
-const TranslationTab = ({formData, setFormData, getFormData, ...props}) => {
-  const [
-    touched,
-    handleTouched,
-    addValidity,
-    formIsValid
-  ] = useTabForm({fields: {0:false, 1:false}, setIsValid: props.setIsValid});
-
-
-  const formatCodeAndDescription = (data) => `${data.descripcio} (${data.codi})`;
-
+  const formatCodeAndDescription = (data) =>
+    `${data.descripcio} (${data.codi})`;
 
   const { id: articleId } = useParams();
-
 
   const translation = {
     title: props.intl.formatMessage({
@@ -43,8 +35,8 @@ const TranslationTab = ({formData, setFormData, getFormData, ...props}) => {
       },
     ],
     extraPostBody: {
-        article: { id: articleId},
-      },
+      article: { id: articleId },
+    },
 
     columns: [
       {
@@ -55,6 +47,13 @@ const TranslationTab = ({formData, setFormData, getFormData, ...props}) => {
         }),
         getCellValue: (row) => row?.idioma?.description ?? "",
       },
+      {
+        name: "descripcioCurta",
+        title: props.intl.formatMessage({
+          id: "Articulos.descCorta",
+          defaultMessage: "Descripción Corta",
+        }),
+      },
 
       {
         name: "descripcio",
@@ -62,7 +61,6 @@ const TranslationTab = ({formData, setFormData, getFormData, ...props}) => {
           id: "Comun.descripcion",
           defaultMessage: "Descripción",
         }),
-    
       },
     ],
     formComponents: [
@@ -73,20 +71,31 @@ const TranslationTab = ({formData, setFormData, getFormData, ...props}) => {
         }),
         type: "LOV",
         key: "idioma",
-        id:"idiomas",
+        id: "idiomas",
         noEditable: true,
         breakpoints: {
           xs: 12,
           md: 3,
         },
         selector: {
-            key: 'idiomas',
-            labelKey: formatCodeAndDescription,
-            sort: 'descripcio',
+          key: "idiomas",
+          labelKey: formatCodeAndDescription,
+          sort: "descripcio",
         },
         validationType: "object",
- 
+      },
+      {
+        placeHolder: props.intl.formatMessage({
+          id: "Articulos.descCorta",
+          defaultMessage: "Descripción Corta",
+        }),
+        type: "input",
+        key: "descripcioCurta",
 
+        breakpoints: {
+          xs: 12,
+          md: 9,
+        },
       },
       {
         placeHolder: props.intl.formatMessage({
@@ -95,39 +104,39 @@ const TranslationTab = ({formData, setFormData, getFormData, ...props}) => {
         }),
         type: "input",
         key: "descripcio",
-  
+
         breakpoints: {
           xs: 12,
-          md: 9,
+          md: 12,
         },
-        validationType: "string",
-        text:{
-          multiline: 10,
-        }
-    
+        text: {
+          multiline: 8,
+        },
       },
     ],
   };
 
-
   return (
-    <Grid container >
+    <Grid container>
       <Grid xs={12} item>
-        <OutlinedContainer className="general-tab-container" title={<FormattedMessage id={"Articulos.tabs.traducciones"} defaultMessage={"Traudcciones"}/>}>
-        <ExpandableGrid
+        <OutlinedContainer
+          className="general-tab-container"
+          title={
+            <FormattedMessage
+              id={"Articulos.tabs.traducciones"}
+              defaultMessage={"Traudcciones"}
+            />
+          }
+        >
+          <ExpandableGrid
             id="articleTraduccios"
             responseKey="articleTraduccios"
             enabled={props.editMode}
             configuration={translation}
           />
-          </OutlinedContainer>
+        </OutlinedContainer>
       </Grid>
-   
     </Grid>
   );
 };
-export default compose(
-  React.memo,
-  withValidations,
-  injectIntl
-)(TranslationTab);
+export default compose(React.memo, withValidations, injectIntl)(TranslationTab);
