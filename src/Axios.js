98f6b2ba73a7739733c5327407189a99d333a4f4
amula-limit <amula@limit.es>
2021-07-22 09:30:55 +0200

    Merge Development

--- src/Axios.js
+++ src/Axios.js
@@ -11,7 +11,6 @@ const authToken = () => 'Bearer ' + getPlainFrom(TOKEN_LOCALSTORAGE_KEY);
 
 //Axios.defaults.baseURL = config.apiRoot;
 Axios.defaults.baseURL = 'https://10.35.3.44:8083/';
-//'';
 
 Axios.interceptors.request.use(function (conf) {
   // Do something before request is sent
