8b0f66cca057becddc55943e1adc47c5ef99b2b4
amula-limit <amula@limit.es>
2021-08-03 08:32:10 +0200

    Añadir tab numeracionesTpv a fact/PuntoVenta

--- src/pages/fact/PointSale/NumeracionesTab.js
+++ src/pages/fact/PointSale/NumeracionesTab.js
@@ -81,39 +81,52 @@ const NumeracionesTab = ({ formData, setFormData, getFormData, ...props }) => {
 
     columns: [
       {
-        name: "aplicacio",
+        name: "dia",
         title: props.intl.formatMessage({
-          id: "Clientes.aplicacion",
-          defaultMessage: "Aplicación",
+          id: "PuntoVenta.Fecha",
+          defaultMessage: "Fecha",
         }),
       },
       {
-        name: "aplicacioCodi",
+        name: "ultimTicket",
         title: props.intl.formatMessage({
-          id: "Clientes.codigoOtraApp",
-          defaultMessage: "Código Cliente otra App",
+          id: "PuntoVenta.ultimoTicket",
+          defaultMessage: "Último ticket",
         }),
       },
       {
-        name: "percentatge",
+        name: "numeroAZ",
         title: props.intl.formatMessage({
-          id: "Clientes.porcentaje",
-          defaultMessage: "Porcentaje",
+          id: "PuntoVenta.numeroAZ",
+          defaultMessage: "Numero de A-Z",
         }),
       },
       {
-        name: "empresa",
+        name: "valorNumeroAZ",
         title: props.intl.formatMessage({
-          id: "Clientes.empresas",
-          defaultMessage: "Empresa",
+          id: "PuntoVenta.importe",
+          defaultMessage: "Importe",
         }),
-        getCellValue: (row) => row.empresa && row.empresa?.description,
       },
       {
-        name: "observacions",
+        name: "exerciciComptable",
         title: props.intl.formatMessage({
-          id: "Comun.observaciones",
-          defaultMessage: "Observaciones",
+          id: "PuntoVenta.ejercicio",
+          defaultMessage: "Ejercicio",
+        }),
+      },
+      {
+        name: "diariComptable",
+        title: props.intl.formatMessage({
+          id: "PuntoVenta.diario",
+          defaultMessage: "Diario",
+        }),
+      },
+      {
+        name: "seientComptable",
+        title: props.intl.formatMessage({
+          id: "PuntoVenta.asiento",
+          defaultMessage: "Asiento",
         }),
       },
     ],
@@ -121,508 +134,167 @@ const NumeracionesTab = ({ formData, setFormData, getFormData, ...props }) => {
     formComponents: [
       {
         placeHolder: props.intl.formatMessage({
-          id: "Clientes.aplicacion",
-          defaultMessage: "Aplicación",
+          id: "PuntoVenta.fecha",
+          defaultMessage: "Fecha",
         }),
-        type: "input",
-        key: "aplicacio",
-        noEditable: true,
-        required: true,
+        type: "date",
+        key: "dia",
+        required:true,
         breakpoints: {
           xs: 12,
           md: 3,
         },
-        validationType: "string",
-        ...withRequiredValidation([
-          ...props.stringValidations.minMaxValidation(0, 3),
-        ]),
+        validationType:"string",
+        validations: [...props.commonValidations.requiredValidation() ]
       },
       {
         placeHolder: props.intl.formatMessage({
-          id: "Clientes.codigoOtraApp",
-          defaultMessage: "Código cliente otra Aplicación",
-        }),
-        type: "input",
-        key: "aplicacioCodi",
-        noEditable: true,
-        required: true,
-        breakpoints: {
-          xs: 12,
-          md: 3,
-        },
-        validationType: "string",
-        ...withRequiredValidation([
-          ...props.stringValidations.minMaxValidation(0, 10),
-        ]),
-      },
-      {
-        placeHolder: props.intl.formatMessage({
-          id: "Clientes.porcentaje",
-          defaultMessage: "Porcentaje ",
+          id: "PuntoVenta.ultimoTicket",
+          defaultMessage: "Último Ticket",
         }),
         type: "numeric",
-        key: "percentatge",
-        noEditable: true,
+        key: "ultimTicket",
+
         breakpoints: {
           xs: 12,
-          md: 2,
+          md: 3,
         },
-        suffix: "%",
-        validationType: "number",
-        validations: [...props.numberValidations.minMaxValidation(0, 9999)],
+        validationType:"number",
+        validations: [...props.numberValidations.minMaxValidation(0,9999999999) ]
       },
       {
         placeHolder: props.intl.formatMessage({
-          id: "Clientes.empresas",
-          defaultMessage: "Empresas",
+          id: "PuntoVenta.numeroAZ",
+          defaultMessage: "numeroAZ",
         }),
-        type: "LOV",
-        key: "empresa",
-        required: true,
+        type: "numeric",
+        key: "numeroAZ",
+
         breakpoints: {
           xs: 12,
-          md: 4,
+          md: 3,
         },
-        selector: {
-          key: "empresas",
-          labelKey: (data) => `${data.nomFiscal} (${data.codi})`,
-          sort: "nomFiscal",
-          cannotCreate: true,
-          advancedSearchColumns: [
-            {
-              title: props.intl.formatMessage({
-                id: "Comun.codigo",
-                defaultMessage: "Código",
-              }),
-              name: "codi",
-            },
-            {
-              title: props.intl.formatMessage({
-                id: "Comun.nombre",
-                defaultMessage: "Nombre",
-              }),
-              name: "nomFiscal",
-            },
-          ],
-        },
+        validationType:"number",
+        validations: [...props.numberValidations.minMaxValidation(0,9999999999) ]
       },
       {
         placeHolder: props.intl.formatMessage({
-          id: "Comun.observaciones",
-          defaultMessage: "Observaciones",
+          id: "PuntoVenta.importe",
+          defaultMessage: "Importe",
+        }),
+        required:true,
+        type: "numeric",
+        key: "valorNumeroAZ",
+
+        breakpoints: {
+          xs: 12,
+          md: 3,
+        },
+        validationType:"number",
+        validations: [...props.commonValidations.requiredValidation(),
+          ...props.numberValidations.minMaxValidation(0,999999999999) ]
+      },
+      {
+        placeHolder: props.intl.formatMessage({
+          id: "PuntoVenta.valorNumeroAZ2",
+          defaultMessage: "Valor Numero AZ 2",
+        }),
+        required:true,
+        type: "numeric",
+        key: "valorNumeroAZ002",
+
+        breakpoints: {
+          xs: 12,
+          md: 3,
+        },
+        validationType:"number",
+        validations: [...props.commonValidations.requiredValidation(),
+          ...props.numberValidations.minMaxValidation(0,999999999999) ]
+      },
+      {
+        placeHolder: props.intl.formatMessage({
+          id: "PuntoVenta.ejercicio",
+          defaultMessage: "Ejercicio",
         }),
         type: "input",
-        key: "observacions",
+        key: "exerciciComptable",
+
         breakpoints: {
           xs: 12,
-          md: 12,
+          md: 3,
         },
+        validationType:"string",
+        validations: [...props.stringValidations.minMaxValidation(0,4) ]
       },
+      {
+        placeHolder: props.intl.formatMessage({
+          id: "PuntoVenta.diairo",
+          defaultMessage: "Diario",
+        }),
+        type: "input",
+        key: "diariComptable",
+
+        breakpoints: {
+          xs: 12,
+          md: 3,
+        },
+        validationType:"string",
+        validations: [...props.stringValidations.minMaxValidation(0,2) ]
+      },
+      {
+        placeHolder: props.intl.formatMessage({
+          id: "PuntoVenta.asiento",
+          defaultMessage: "Asiento",
+        }),
+        type: "numeric",
+        key: "seienComptable",
+
+        breakpoints: {
+          xs: 12,
+          md: 3,
+        },
+        validationType:"number",
+        validations: [...props.numberValidations.minMaxValidation(0,9999999999) ]
+      },
+      {
+        placeHolder: props.intl.formatMessage({
+          id: "Proveedores.divisa",
+          defaultMessage: "Divisa",
+        }),
+        type: "LOV",
+        key: "divisa",
+        breakpoints: {
+          xs: 12,
+          md: 3,
+        },
+        selector: {
+          key: "divisas",
+          labelKey: formatCodeAndName,
+          sort: "nom",
+          advancedSearchColumns: aSCodeAndName,
+          cannotCreate:true,
+        },
+
+      },
+      {
+        placeHolder: props.intl.formatMessage({
+          id: "PuntoVenta.valorDivisa",
+          defaultMessage: "Valor Divisa",
+        }),
+        type: "numeric",
+        key: "valorDivisaEuros",
+        suffix:"€",
+        breakpoints: {
+          xs: 12,
+          md: 3,
+        },
+        validationType:"number",
+        validations: [...props.numberValidations.minMaxValidation(0,9999999999) ]
+      },
+     
     ],
   };
 
-  const createConfiguration = [
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "Comun.codigo",
-        defaultMessage: "Código",
-      }),
-      type: "input",
-      key: "codi",
-      required: true,
-      breakpoints: {
-        xs: 12,
-        md: 2,
-      },
-      noEditable: true,
-      validationType: "string",
-      validations: [
-        ...props.commonValidations.requiredValidation(),
-        ...props.stringValidations.minMaxValidation(1, 4),
-        ...props.stringValidations.fieldExistsValidation(
-          "puntsVenda",
-          "codi",
-          props.intl.formatMessage({
-            id: "Comun.codigo",
-            defaultMessage: "Código",
-          })
-        ),
-      ],
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "Comun.nombre",
-        defaultMessage: "Nombre",
-      }),
-      type: "input",
-      key: "nom",
-      required: true,
-      breakpoints: {
-        xs: 12,
-        md: 3,
-      },
-      validationType: "string",
-      validations: [
-        ...props.commonValidations.requiredValidation(),
-        ...props.stringValidations.minMaxValidation(1, 30),
-      ],
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.codigoAperturaCajon",
-        defaultMessage: "Código apertura Cajón",
-      }),
-      type: "input",
-      key: "codiAperturaCaixa",
-
-      breakpoints: {
-        xs: 12,
-        md: 5,
-      },
-      validationType: "string",
-      validations: [...props.stringValidations.minMaxValidation(0, 60)],
-    },
-
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.liniasSaltoFinal",
-        defaultMessage: "Linias Salto Final Ticket ",
-      }),
-      required: true,
-      type: "numeric",
-      key: "ticketNumLiniesEnBlancFinal",
-
-      breakpoints: {
-        xs: 12,
-        md: 2,
-      },
-      validationType: "number",
-      validations: [
-        ...props.commonValidations.requiredValidation(),
-        ...props.numberValidations.minMaxValidation(0, 99),
-      ],
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "Presupuestos.cliente",
-        defaultMessage: "Cliente",
-      }),
-      type: "LOV",
-      key: "client",
-      required: true,
-      breakpoints: {
-        xs: 12,
-        md: 4,
-      },
-      selector: {
-        key: "clients",
-        labelKey: (data) => `${data.nomComercial} (${data.codi})`,
-        sort: "descripcio",
-        cannotCreate: true,
-        advancedSearchColumns: [
-          { title: CODE, name: "codi" },
-          { title: NOM, name: "nomComercial" },
-        ],
-      },
-      validationType: "object",
-      validations: [...props.commonValidations.requiredValidation()],
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.cajas",
-        defaultMessage: "Cajas",
-      }),
-      type: "LOV",
-      key: "caixa",
-      required: true,
-      breakpoints: {
-        xs: 12,
-        md: 3,
-      },
-      selector: {
-        key: "caixas",
-        labelKey: formatCodeAndDescription,
-        sort: "descripcio",
-        cannotCreate: true,
-        advancedSearchColumns: aSCodeAndDescription,
-      },
-      validationType: "object",
-      validations: [...props.commonValidations.requiredValidation()],
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "Presupuestos.almacen",
-        defaultMessage: "Almacén",
-      }),
-      type: "LOV",
-      key: "magatzem",
-      required: true,
-      breakpoints: {
-        xs: 12,
-        md: 3,
-      },
-      selector: {
-        key: "magatzems",
-        labelKey: formatCodeAndName,
-        sort: "nom",
-        cannotCreate: true,
-        advancedSearchColumns: aSCodeAndName,
-      },
-      validationType: "object",
-      validations: [...props.commonValidations.requiredValidation()],
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.cortePapel",
-        defaultMessage: "Corte Papel",
-      }),
-      type: "input",
-      key: "tallPaper",
-      breakpoints: {
-        xs: 12,
-        md: 2,
-      },
-      validationType: "string",
-      validations: [...props.stringValidations.minMaxValidation(0, 15)],
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "Presupuestos.documentoPago",
-        defaultMessage: "Documento pago/cobro",
-      }),
-      type: "LOV",
-      key: "documentPagamentCobrament",
-      required: true,
-      breakpoints: {
-        xs: 12,
-        md: 4,
-      },
-      selector: {
-        key: "documentPagamentCobraments",
-        labelKey: formatCodeAndDescription,
-        sort: "descripcio",
-        cannotCreate: true,
-        advancedSearchColumns: aSCodeAndDescription,
-      },
-      validationType: "object",
-      validations: [...props.commonValidations.requiredValidation()],
-    },
-
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "Presupuestos.divisa",
-        defaultMessage: "Divisa",
-      }),
-      type: "LOV",
-      key: "divisa",
-      required: true,
-      breakpoints: {
-        xs: 12,
-        md: 3,
-      },
-      selector: {
-        key: "divisas",
-        labelKey: formatCodeAndName,
-        sort: "descripcio",
-        cannotCreate: true,
-        advancedSearchColumns: aSCodeAndName,
-      },
-      validationType: "object",
-      validations: [...props.commonValidations.requiredValidation()],
-    },
-
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "Presupuestos.operario",
-        defaultMessage: "Operario",
-      }),
-      type: "LOV",
-      key: "operari",
-      breakpoints: {
-        xs: 12,
-        md: 3,
-      },
-      selector: {
-        key: "operaris",
-        labelKey: formatCodeAndName,
-        sort: "nom",
-        cannotCreate: true,
-        advancedSearchColumns: aSCodeAndName,
-      },
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.ultimoAZ",
-        defaultMessage: "Último AZ",
-      }),
-      type: "numeric",
-      key: "darrerAz",
-      breakpoints: {
-        xs: 12,
-        md: 2,
-      },
-      validationType: "number",
-      validations: [...props.numberValidations.minMaxValidation(0, 2147483648)],
-    },
-
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "Presupuestos.serieVenta",
-        defaultMessage: "Serie Venta",
-      }),
-      type: "LOV",
-      key: "serie",
-      breakpoints: {
-        xs: 12,
-        md: 2,
-      },
-      selector: {
-        key: "serieVendas",
-        labelKey: formatCodeAndDescription,
-        sort: "descripcio",
-        cannotCreate: true,
-        advancedSearchColumns: aSCodeAndDescription,
-      },
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "Presupuestos.divisaSecundaria",
-        defaultMessage: "Divisa",
-      }),
-      type: "LOV",
-      key: "divisaSecundaria",
-      id: "divisa",
-      breakpoints: {
-        xs: 12,
-        md: 2,
-      },
-      selector: {
-        key: "divisas",
-        labelKey: formatCodeAndName,
-        sort: "descripcio",
-        cannotCreate: true,
-        advancedSearchColumns: aSCodeAndName,
-      },
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.tipoImpresion",
-        defaultMessage: "Tipo impresión",
-      }),
-      type: "select",
-      key: "impressioTipus",
-      required: true,
-      breakpoints: {
-        xs: 12,
-        md: 2,
-      },
-      selector: {
-        options: TIPO_IMPRESION_PUNT_VENTA_VALUES,
-      },
-      validationType: "string",
-      validations: [...props.commonValidations.requiredValidation()],
-    },
-    // {
-    //   placeHolder: props.intl.formatMessage({
-    //     id: "PuntoVenta.horaInicio",
-    //     defaultMessage: "Hora Inicio Día",
-    //   }),
-    //   type: "date",
-    //   key: "horaIniciDia",
-
-    //   breakpoints: {
-    //     xs: 12,
-    //     md: 2,
-    //   },
-    // },
-
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.direccionIp",
-        defaultMessage: "Dirección IP",
-      }),
-      type: "input",
-      key: "adreçaIp",
-      breakpoints: {
-        xs: 12,
-        md: 4,
-      },
-      validationType: "string",
-      validations: [...props.stringValidations.minMaxValidation(0, 60)],
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.nombreBdTpv",
-        defaultMessage: "Nombre BD TPV",
-      }),
-      type: "input",
-      key: "tpvBaseDadesNom",
-      breakpoints: {
-        xs: 12,
-        md: 2,
-      },
-      validationType: "string",
-      validations: [...props.stringValidations.minMaxValidation(0, 20)],
-    },
-
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.carpetasImg",
-        defaultMessage: "Carpetas Imágenes",
-      }),
-      type: "input",
-      key: "carpetaImatges",
-
-      breakpoints: {
-        xs: 12,
-        md: 4,
-      },
-      validationType: "string",
-      validations: [...props.stringValidations.minMaxValidation(0, 80)],
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.carpetasTpv",
-        defaultMessage: "Carpetas TPV",
-      }),
-      type: "input",
-      key: "tpvCarpeta",
-
-      breakpoints: {
-        xs: 12,
-        md: 4,
-      },
-      validationType: "string",
-      validations: [...props.stringValidations.minMaxValidation(0, 80)],
-    },
-
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.fechaImpresion",
-        defaultMessage: "Fecha impresión",
-      }),
-      type: "date",
-      key: "dataImp",
-
-      breakpoints: {
-        xs: 12,
-        md: 2,
-      },
-    },
-    {
-      placeHolder: props.intl.formatMessage({
-        id: "PuntoVenta.ticketIva",
-        defaultMessage: "Ticket IVA incluido",
-      }),
-      type: "checkbox",
-      key: "ticketIvaInclos",
-
-      breakpoints: {
-        xs: 12,
-        md: 2,
-      },
-    },
-  ];
 
   return (
     <Grid container>
