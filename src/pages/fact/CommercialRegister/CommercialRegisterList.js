5cf57dd8e562252b7251afb7de9e7ff0721647b7
amula-limit <amula@limit.es>
2021-08-26 08:53:03 +0200

    Cambio en RegistroComericalesList

--- src/pages/fact/CommercialRegister/CommercialRegisterList.js
+++ src/pages/fact/CommercialRegister/CommercialRegisterList.js
@@ -26,6 +26,72 @@ const CommercialRegisterList = ({ actions, ...props }) => {
     ]);
   }, []);
 
+  const CODE = props.intl.formatMessage({
+    id: "Comun.codigo",
+    defaultMessage: "Código",
+  });
+
+  const NOM = props.intl.formatMessage({
+    id: "Comun.nombre",
+    defaultMessage: "Nombre",
+  });
+
+  const aSCodeAndName = [
+    { title: CODE, name: "codi" },
+    { title: NOM, name: "nom" },
+  ];
+
+  const NOM_COMERCIAL = props.intl.formatMessage({
+    id: "Proveedores.nombre_comercial",
+    defaultMessage: "Nombre Comercial",
+  });
+
+  const aSCodeAndNameComercial = [
+    { title: CODE, name: "codi" },
+    { title: NOM_COMERCIAL, name: "nomComercial" },
+  ];
+
+
+  const cliente =     {
+    placeHolder: props.intl.formatMessage({
+      id: "Clientes.titulo",
+      defaultMessage: "Clientes"
+    }),
+    type: 'LOV',
+    key: 'client',
+    breakpoints: {
+      xs: 12,
+      md: 5
+    },
+    selector: {
+      key: "clients",
+      labelKey: (data) => `${data.nomComercial} (${data.codi})`,
+      sort: 'codi',
+      cannotCreate: true,
+      advancedSearchColumns: aSCodeAndNameComercial
+    },
+  };
+
+  const producto =  {
+    placeHolder: props.intl.formatMessage({
+      id: "RegistroComercial.productos",
+      defaultMessage: "Productos"
+    }),
+    type: 'LOV',
+    key: 'producte',
+    breakpoints: {
+      xs: 12,
+      md: 4
+    },
+    selector: {
+      key: "productes",
+      labelKey: (data) => `${data.nom} (${data.codi})`,
+      sort: 'codi',
+      cannotCreate: true,
+      advancedSearchColumns: aSCodeAndName
+    },
+  };
+
   const listConfiguration = {
     columns: [
       {
@@ -42,9 +108,28 @@ const CommercialRegisterList = ({ actions, ...props }) => {
           defaultMessage: "Descripción del Medio",
         }),
       },
+      {
+        name: "client",
+        title: props.intl.formatMessage({
+          id: "Clientes.titulo",
+          defaultMessage: "Clientes"
+        }),
+        getCellValue: (row) => (row.client ? row.client.description : ""),
+        field:cliente
+      },
+      {
+        name: "producte",
+        title:  props.intl.formatMessage({
+          id: "RegistroComercial.productos",
+          defaultMessage: "Productos"
+        }),
+        getCellValue: (row) => (row.producte ? row.producte.description : ""),
+        field:producto
+      },
     ],
     URL: API.registreComercial,
     listKey: "registreComercials",
+    enableInlineEdition: true
   };
   return <ReactGrid id="registreComercial" configuration={listConfiguration} />;
 };
