826cb228608eff6393a5304e9a1371859f28ded7
amula-limit <amula@limit.es>
2021-08-26 08:29:17 +0200

    Cambio en OficinaBancariaList

--- src/pages/fact/BankOffice/BankOfficeList.js
+++ src/pages/fact/BankOffice/BankOfficeList.js
@@ -26,6 +26,144 @@ const BankOfficeList = ({ actions, ...props }) => {
     ]);
   }, []);
 
+  const CODE = props.intl.formatMessage({
+    id: "Comun.codigo",
+    defaultMessage: "Código",
+  });
+  const DESCRIPCIO = props.intl.formatMessage({
+    id: "Comun.descripcion",
+    defaultMessage: "Descripción",
+  });
+
+  const NOM = props.intl.formatMessage({
+    id: "Comun.nombre",
+    defaultMessage: "Nombre",
+  });
+
+
+
+  const code = (md = 6) => ({
+    type: "input",
+    key: "codi",
+    placeHolder: CODE,
+    required: true,
+    noEditable: true,
+    breakpoints: {
+      xs: 12,
+      md: md,
+    },
+  });
+
+
+  const aSCodeAndDescription = [
+    { title: CODE, name: "codi" },
+    { title: DESCRIPCIO, name: "descripcio" },
+  ];
+  const aSCodeAndName = [
+    { title: CODE, name: "codi" },
+    { title: NOM, name: "nom" },
+  ];
+
+
+  const codiPostal  =
+    {
+      placeHolder: props.intl.formatMessage({
+        id: "Proveedores.Direccion.codPostal",
+        defaultMessage: "Código Postal",
+      }),
+      type: "LOV",
+      key: "codiPostal",
+      breakpoints: {
+        xs: 12,
+        md: 6,
+      },
+      selector: {
+        key: "codiPostals",
+        labelKey: (data) =>
+          `${data.poblacio} ${data.municipi ? ` - ${data.municipi}` : ""} (${
+            data.codi
+          })`,
+        sort: "codi",
+        creationComponents: [
+          code(4),
+          {
+            placeHolder: props.intl.formatMessage({
+              id: "CodigoPostal.pais",
+              defaultMessage: "País",
+            }),
+            type: "LOV",
+            key: "pais",
+            required: false,
+            breakpoints: {
+              xs: 12,
+              md: 4,
+            },
+            selector: {
+              key: "paises",
+              labelKey: (data) => `${data.nom} (${data.codi})`,
+              sort: "codi",
+              cannotCreate: true,
+              relatedWith: [
+                {
+                  name: "provincia",
+                  filterBy: "pais.id",
+                  keyValue: "id",
+                },
+              ],
+              advancedSearchColumns: aSCodeAndName,
+            },
+          },
+          {
+            placeHolder: props.intl.formatMessage({
+              id: "CodigoPostal.provincia",
+              defaultMessage: "Provincia",
+            }),
+            type: "LOV",
+            key: "provincia",
+            required: false,
+            breakpoints: {
+              xs: 12,
+              md: 4,
+            },
+            selector: {
+              key: "provincias",
+              labelKey: (data) => `${data.nom} (${data.codi})`,
+              sort: "codi",
+              cannotCreate: true,
+              advancedSearchColumns: aSCodeAndName,
+            },
+          },
+          {
+            type: "input",
+            key: "municipi",
+            placeHolder: props.intl.formatMessage({
+              id: "CodigoPostal.municipio",
+              defaultMessage: "Municipio",
+            }),
+            required: true,
+            breakpoints: {
+              xs: 12,
+              md: 6,
+            },
+          },
+          {
+            type: "input",
+            key: "poblacio",
+            placeHolder: props.intl.formatMessage({
+              id: "CodigoPostal.poblacion",
+              defaultMessage: "Población",
+            }),
+            required: true,
+            breakpoints: {
+              xs: 12,
+              md: 6,
+            },
+          },
+        ],
+        advancedSearchColumns: aSCodeAndDescription,
+      },
+    };
+
   const listConfiguration = {
     columns: [
       {
@@ -34,6 +172,7 @@ const BankOfficeList = ({ actions, ...props }) => {
           id: "Comun.codigo",
           defaultMessage: "Código",
         }),
+        inlineEditionDisabled: true
       },
       {
         name: "domicili",
@@ -48,7 +187,8 @@ const BankOfficeList = ({ actions, ...props }) => {
           id: "Comun.codigoPostal",
           defaultMessage: "Código Postal",
         }),
-        getCellValue: row => row.codiPostal?.description ?? ""
+        getCellValue: row => row.codiPostal?.description ?? "",
+        field: codiPostal
       },
       {
         name: "banc",
@@ -56,7 +196,8 @@ const BankOfficeList = ({ actions, ...props }) => {
           id: "OficinasBancarias.banco",
           defaultMessage: "Banco",
         }),
-        getCellValue: row => row.banc?.description ?? ""
+        getCellValue: row => row.banc?.description ?? "",
+        inlineEditionDisabled: true
       },
       {
         name: "telefon",
@@ -69,6 +210,7 @@ const BankOfficeList = ({ actions, ...props }) => {
     ],
     URL: API.oficinaBancaria,
     listKey: "oficinaBancarias",
+    enableInlineEdition: true
   };
   return <ReactGrid id="oficinaBancaria" configuration={listConfiguration} />;
 };
