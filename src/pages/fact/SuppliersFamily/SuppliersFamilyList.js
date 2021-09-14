06cb667a7a317c79d5d96cdccca404b69fcc3b18
amula-limit <amula@limit.es>
2021-09-14 13:41:03 +0200

    Cambios en Familia Proveedores

--- src/pages/fact/SuppliersFamily/SuppliersFamilyList.js
+++ src/pages/fact/SuppliersFamily/SuppliersFamilyList.js
@@ -1,64 +1,102 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import ReactGrid from "../../../modules/ReactGrid";
-import {bindActionCreators,compose} from "redux";
-import {setBreadcrumbHeader, setListingConfig} from "../../../redux/pageHeader";
-import {injectIntl} from "react-intl";
-import {connect} from "react-redux";
+import { bindActionCreators, compose } from "redux";
+import {
+  setBreadcrumbHeader,
+  setListingConfig,
+} from "../../../redux/pageHeader";
+import { injectIntl } from "react-intl";
+import { connect } from "react-redux";
 import * as API from "redux/api";
 
 const SuppliersFamilyList = ({ actions, ...props }) => {
-
   useEffect(() => {
     actions.setListingConfig({
       title: props.intl.formatMessage({
         id: "FamiliaProveedores.titulo",
-        defaultMessage: "Familias proveedor"
+        defaultMessage: "Familias proveedor",
       }),
     });
     actions.setBreadcrumbHeader([
-      {title: props.intl.formatMessage({
+      {
+        title: props.intl.formatMessage({
           id: "FamiliaProveedores.titulo",
-          defaultMessage: "Familias proveedor"
-        }), href:"/fact/familia-proveedores"}
+          defaultMessage: "Familias proveedor",
+        }),
+        href: "/fact/familia-proveedores",
+      },
     ]);
-  },[]);
+  }, []);
 
   const listConfiguration = {
     columns: [
-      { name: 'codi',
+      {
+        name: "codi",
         title: props.intl.formatMessage({
           id: "FamiliaProveedores.codigo",
-          defaultMessage: "Código"
+          defaultMessage: "Código",
         }),
-        inlineEditionDisabled: true
+        inlineEditionDisabled: true,
       },
-      { name: 'nom',
+      {
+        name: "nom",
         title: props.intl.formatMessage({
           id: "FamiliaProveedores.nombre",
-          defaultMessage: "Nombre"
-        })
+          defaultMessage: "Nombre",
+        }),
+      },
+      {
+        name: "ctacprcmp",
+        title: props.intl.formatMessage({
+          id: "FamiliaProveedores.ctaprcmp",
+          defaultMessage: "Cuenta Compras",
+        }),
+      },
+      {
+        name: "tipasicmp",
+        title: props.intl.formatMessage({
+          id: "FamiliaProveedores.tipasicmp",
+          defaultMessage: "Tipo Asig. Fac",
+        }),
+      },
+      {
+        name: "dricmp",
+        title: props.intl.formatMessage({
+          id: "FamiliaProveedores.dricmp",
+          defaultMessage: "Diario Fac",
+        }),
+      },
+      {
+        name: "dricmp",
+        title: props.intl.formatMessage({
+          id: "FamiliaProveedores.driprfcmp",
+          defaultMessage: "Diario Prof.",
+        }),
+      },
+      {
+        name: "observacions",
+        title: props.intl.formatMessage({
+          id: "Comun.observaciones",
+          defaultMessage: "Observaciones",
+        }),
       },
     ],
     URL: API.familiaProveidor,
-    listKey: 'familiaProveidors',
-    enableInlineEdition: true
+    listKey: "familiaProveidors",
+    enableInlineEdition: true,
   };
-  return (
-    <ReactGrid
-      id='familiaProveidor'
-      configuration={listConfiguration} />
-  );
-}
+  return <ReactGrid id="familiaProveidor" configuration={listConfiguration} />;
+};
 
 const mapDispatchToProps = (dispatch, props) => {
   const actions = {
     setListingConfig: bindActionCreators(setListingConfig, dispatch),
-    setBreadcrumbHeader: bindActionCreators(setBreadcrumbHeader, dispatch)
+    setBreadcrumbHeader: bindActionCreators(setBreadcrumbHeader, dispatch),
   };
   return { actions };
 };
 
 export default compose(
   injectIntl,
-  connect(null,mapDispatchToProps)
+  connect(null, mapDispatchToProps)
 )(SuppliersFamilyList);
